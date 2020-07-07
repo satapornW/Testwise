20f7f376801b41bc8cf6f9a2d213159dffe71136
satapornW <60197610+satapornW@users.noreply.github.com>
2020-07-07 09:30:58 -0700

    Update to broken URL Test, run each test in different main function

--- cypress/integration/Likewise Test/function/brokenUrl.js
+++ cypress/integration/Likewise Test/function/brokenUrl.js
@@ -3,18 +3,23 @@
 var badPath = 'profile/tuesday_morning_lala';
 var badList = 'list/Podcast-list-for-than-6-5ec4059e94955800243f5a1dkjjkk';
 
-describe('Bad URL', function(){
+describe('Bad URL Test 1', function(){
 	it('Broken Profile URL', function(){
+
 		cy.startUp();
 		cy.visit('/' + badPath);
 		cy.get('h1[class="text-semibold align-center"]').should('contain', 'Sorry');
-
+		
 	})
+})
 
+describe('Bad URL Test 2', function(){
 	it('Bad List URL', function(){
+
 		cy.startUp();
 		cy.visit('/' + badList);
 		cy.get('h1[class="error"]').should('contain', 'An Error has occurred loading list details');
+	
 	})
 
-} )
+})
