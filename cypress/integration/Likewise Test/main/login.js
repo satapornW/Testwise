0609e7ebb714b5d3b6ed9adbfa08bab1d15b12e8
satapornW <60197610+satapornW@users.noreply.github.com>
2020-07-28 17:42:30 -0700

    update login

--- cypress/integration/Likewise Test/main/login.js
+++ cypress/integration/Likewise Test/main/login.js
@@ -22,6 +22,72 @@ describe('login modal and action', function(){
 			should('contain', 'Continue with Google');
 		cy.get('button[class="lw-btn apple rounded mat-flat-button ng-trigger ng-trigger-loading"]').
 			should('contain', 'Continue with Apple');
+
+	})
+
+	it('Invalid password', function(){
+		cy.get('input[name="email"]').type('accdc@yahoo.com');
+	    cy.get('input[name="password"]').type('11110000');
+	    cy.get('button[class="lw-btn align-center secondary rounded mat-flat-button ng-trigger ng-trigger-loading primary"]').click();
+	    
+	    cy.get('div[class="lw-label error mb-10px ng-star-inserted"]')
+	    	.should('contain',' The username and password you entered did not match our records. Please double-check and try again.');
+	    
+	    cy.get('input[name="email"]').clear();
+	    cy.get('input[name="password"]').clear();
+		
+	})
+
+	it('Invalid login', function(){
+
+		cy.get('input[name="email"]').type('apolsnn@losksooaj.ccoomm');
+	    cy.get('input[name="password"]').type('11110000');
+	    cy.get('button[class="lw-btn align-center secondary rounded mat-flat-button ng-trigger ng-trigger-loading primary"]').click();
+	    
+	    cy.get('div[class="lw-label error mb-10px ng-star-inserted"]')
+	    	.should('contain',' The username and password you entered did not match our records. Please double-check and try again.');
+	    
+	    cy.get('input[name="email"]').clear();
+	    cy.get('input[name="password"]').clear();
+	})
+
+	it('Valid Login', function(){
+
+	    cy.get('input[name="email"]').type(email);
+	    cy.get('input[name="password"]').type(password);
+	    cy.get('button[class="lw-btn align-center secondary rounded mat-flat-button ng-trigger ng-trigger-loading primary"]').click();
+
+	    //Validate that I actually login
+	    cy.wait(2000);
+	    cy.get('button[class="create-new-button"]').should('contain', 'Create New');
+	    cy.logOut();
+	})
+})
+
+describe('Switch sign-up to login', function(){
+	
+	it('Set up', () => {
+		
+		cy.startUp();
+
+	})
+
+	it('init register modal and Validate', function(){
+
+		cy.contains('Sign Up').click();
+
+		cy.get('h1[class="title signup"]').should('contain',' Sign Up for Likewise for Personalized Picks! ');
+
+		cy.get('button[class="lw-btn facebook rounded mat-flat-button ng-trigger ng-trigger-loading"]').
+			should('contain', 'Continue with Facebook');
+		cy.get('button[class="lw-btn google rounded mat-flat-button ng-trigger ng-trigger-loading"]').
+			should('contain', 'Continue with Google');
+		cy.get('button[class="lw-btn apple rounded mat-flat-button ng-trigger ng-trigger-loading"]').
+			should('contain', 'Continue with Apple');
+		cy.contains('Continue with email');
+
+		cy.get('button[class="lw-link secondary"]').should('contain', 'Sign In').click();
+
 	})
 
 	it('Invalid password', function(){
@@ -60,4 +126,6 @@ describe('login modal and action', function(){
 	    cy.wait(2000);
 	    cy.get('button[class="create-new-button"]').should('contain', 'Create New');
 	})
+
+
 })
